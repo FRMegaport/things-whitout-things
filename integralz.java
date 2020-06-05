8a401de43461f93428befce79a1c29445a883ff5
Luca Modica <luca.modica55@gmail.com>
2020-06-06 01:14:45 +0200

    Adding a biutifful integral calculator (take this, integralcalculator.com !)

---
+++ integralz.java
@@ -0,0 +1,285 @@
+import java.util.Scanner;
+
+public class integralz {
+
+    public static void setIntegralz(int N) {
+
+        boolean ok = true;
+        Scanner in = new Scanner(System.in);
+        int value = 0;
+        int i = 0;
+        int j = 0;
+
+        if (ok) {
+
+            System.out.print("Enter x" + i + ": ");
+            value = in.nextInt();
+            System.out.println("x" + i + " = " + value);
+            i++;
+
+            if (ok) {
+
+                System.out.print("Enter x" + i + ": ");
+                value = in.nextInt();
+                System.out.println("x" + i + " = " + value);
+                i++;
+
+                if (ok) {
+
+                    System.out.print("Enter x" + i + ": ");
+                    value = in.nextInt();
+                    System.out.println("x" + i + " = " + value);
+                    i++;
+
+                    if (ok) {
+
+                        System.out.print("Enter x" + i + ": ");
+                        value = in.nextInt();
+                        System.out.println("x" + i + " = " + value);
+                        i++;
+
+                        if (ok) {
+
+                            System.out.print("Enter x" + i + ": ");
+                            value = in.nextInt();
+                            System.out.println("x" + i + " = " + value);
+                            i++;
+
+                            if (ok) {
+
+                                System.out.print("Enter x" + i + ": ");
+                                value = in.nextInt();
+                                System.out.println("x" + i + " = " + value);
+                                i++;
+
+                                if (ok) {
+
+                                    System.out.print("Enter x" + i + ", just for backup:");
+                                    value = in.nextInt();
+                                    System.out.println("x" + i + " = " + value);
+                                    i++;
+
+                                    if (ok) {
+
+                                        System.out.print("Enter another x" + i + ", just beacuse we care about u:");
+                                        value = in.nextInt();
+                                        System.out.println("x" + i + " = " + value);
+                                        i++;
+
+                                        if (ok) {
+
+                                            System.out.print("Enter the last x" + i
+                                                    + ", modern problems require modern solutions:");
+                                            value = in.nextInt();
+                                            System.out.println("x" + i + " = " + value);
+                                            i++;
+
+                                            if (ok) {
+
+                                                try {
+
+                                                    System.out.println("We aaare ok my brother! ");
+                                                    Thread.sleep(500);
+                                                    System.out.println("Medium points set!");
+                                                    j=0;
+                                                    while(j<7){
+                                                        System.out.print(".");
+                                                        Thread.sleep(300);
+                                                        j++;
+                                                    }
+                                                    System.out.println();
+                                                    System.out.println();
+
+                                                    System.out.println("Bell'uomo muted!");
+                                                    j=0;
+                                                    while(i<7){
+                                                        System.out.print(".");
+                                                        Thread.sleep(300);
+                                                        j++;
+                                                    }
+                                                    System.out.println();
+                                                    System.out.println();
+
+                                                    System.out.println("Also the graph is looking good.");
+                                                    String space = " ";
+                                                    j=0;
+                                                    while(j<7){
+                                                        int l = i;
+                                                        while(l<7){
+                                                            space += "  ";
+                                                        }
+                                                        System.out.print(".");
+                                                        Thread.sleep(300);
+                                                        j++;
+                                                    }
+                                                    System.out.println();
+                                                    System.out.println();
+
+                                                    System.out.print("Well");
+                                                    j=0;
+                                                    while(j<7){
+                                                        System.out.print(".");
+                                                        Thread.sleep(300);
+                                                        j++;
+                                                    }
+                                                    System.out.println();
+                                                    System.out.println();
+
+                                                    if(ok){
+
+                                                        String reply = "";
+                                                        System.out.println("Just one question: do u use this for ur analysis exam?");
+                                                        System.out.println("- 0 = YEAH");
+                                                        System.out.println("- 1 = Hell no I'm just bored");
+                                                        reply = in.next();
+                                                        if(reply.equals("0")){
+
+                                                            System.out.print("Great, so I'm also useful!");
+                                                            j=0;
+                                                            while(j<7){
+                                                                System.out.print(".");
+                                                                Thread.sleep(300);
+                                                                j++;
+                                                            }
+
+                                                        }
+                                                        else if(reply.equals("1")){
+
+                                                            System.out.print("Well, so flex with ur own skilllz bro.");
+                                                            j=0;
+                                                            while(j<7){
+                                                                System.out.print(".");
+                                                                Thread.sleep(300);
+                                                                j++;
+                                                            }
+
+                                                        }
+                                                        else{
+
+                                                            System.out.print("Dooon't cheat me guy, I interpret this as yes yes yes");
+                                                            j=0;
+                                                            while(j<7){
+                                                                System.out.print(".");
+                                                                Thread.sleep(300);
+                                                                j++;
+                                                            }
+
+                                                        }
+
+                                                        if(ok){
+
+                                                            System.out.println("So, after minutes of research, struggles and lots of caffine, the result");
+
+                                                            if(ok){
+
+                                                                j=0;
+                                                                while(j<7){
+                                                                    System.out.print(".");
+                                                                    Thread.sleep(300);
+                                                                    j++;
+                                                                }
+
+                                                                System.out.println();
+
+                                                                j=0;
+                                                                while(j<7){
+                                                                    System.out.print(";) ");
+                                                                    Thread.sleep(300);
+                                                                    j++;
+                                                                }
+
+                                                                System.out.println();
+                                                                System.out.println();
+                                                                System.out.println();
+                                                                System.out.println();
+
+                                                                System.out.print("Is approximated around the value of ");
+                                                                j=0;
+                                                                while(j<7){
+                                                                    System.out.print("\"");
+                                                                    Thread.sleep(200);
+                                                                    j++;
+                                                                }
+                                                                System.out.print("GAY");
+                                                                j=0;
+                                                                while(j<7){
+                                                                    System.out.print("\"");
+                                                                    Thread.sleep(200);
+                                                                    j++;
+                                                                }
+
+                                                                if(ok){
+
+                                                                    System.out.println("Cheers :D");
+                                                                    System.out.println();
+                                                                    System.out.println();
+                                                                    System.out.println();
+                                                                    System.out.println();
+                                                                    System.out.println();
+                                                                    System.out.println();
+
+                                                                }
+
+
+                                                            }
+
+                                                        }
+
+                                                    }
+
+                                                } 
+                                                catch (InterruptedException e) {
+
+                                                    e.printStackTrace();
+
+                                                }
+
+                                            }
+
+                                        }
+
+                                    }
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = 5; // The obvous number of subdivision
+        String func = "";
+        boolean oof = true;
+
+        System.out.print("Enter the function to be integrated: ");
+
+        func = in.next();
+
+        System.out.print("Enter the number of subdivision of ur beautiful integral: ");
+        n = in.nextInt();
+
+        //Nope, the subdivison are 6
+        n = 6;
+        System.out.println("Ok, everyone knows that the subdivisions are always 6, so u work with 6 ok?");
+        System.out.println("Let's go comerade!");
+
+        
+        while(oof){
+            setIntegralz(n);
+        }
+        
+
+    } 
+    
+}
