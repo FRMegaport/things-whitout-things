6bda20df3159c3e28a16a1a947e852d60251bcd6
Federico Radaelli <57858064+FedericoRadaelli@users.noreply.github.com>
2020-06-08 23:25:40 +0200

    this is the recursive one

---
+++ GayR.java
@@ -0,0 +1,9 @@
+public class GayR{
+    public static void gay(String gay){
+          System.out.println(gay);
+          gay("gay");
+    }
+    public static void main (String [] args ){
+     gay("gay");
+    }
+}
