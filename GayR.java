46d9a616a737859beba21dacacd5e9d77a69c5a0
Luca Modica <luca.modica55@gmail.com>
2020-06-09 15:58:32 +0200

    ReGay again

--- GayR.java
+++
@@ -1,9 +0,0 @@
-public class GayR{
-    public static void gay(String gay){
-          System.out.println(gay);
-          gay("gay");
-    }
-    public static void main (String [] args ){
-     gay("gay");
-    }
-}
---
+++ GayRec.java
@@ -0,0 +1,27 @@
+public class GayRec{
+
+    public static void gay(String gay){ 
+		System.out.println(gay); 
+		gay("UR GAY"); 
+	} 
+    public static void main(String[] args){
+        boolean ur_mom_gay = true;
+        while(ur_mom_gay){
+            {
+                {
+                    {
+                        {
+                            {
+                                {
+                                    {
+                                    gay("UR GAY");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            } 
+        }
+    }
+}
