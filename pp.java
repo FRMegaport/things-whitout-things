7d626cce1e475b6954f27d6c2e1ef350c77c02a6
Federico Radaelli <57858064+FedericoRadaelli@users.noreply.github.com>
2020-06-10 22:23:37 +0200

    it's amazing

---
+++ pp.java
@@ -0,0 +1,11 @@
+public class pp{
+    public static void main(String[]args){
+        System.out.println(" ()");
+        System.out.println(" ||");
+        System.out.println(" ||");
+        System.out.println(" ||");
+        System.out.println(" ||");
+        System.out.println("O  O");
+        
+    }
+}
