7e29e483bde1520cca648e17fda3d72ac2e1378e
Lorenzo Pallanza <lorenzo.pallanza.98@gmail.com>
2021-01-05 15:46:13 +0100

    Extremely important stability update
    yes

--- pp.java
+++ pp.java
@@ -1,6 +1,7 @@
 public class pp{
     public static void main(String[]args){
         
+        System.out.println("\t C O C K");
         System.out.println("  ' ");
         System.out.println(" ()");
         System.out.println(" ||");
